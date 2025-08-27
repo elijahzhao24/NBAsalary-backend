1a83120e82811136a0ebaadb820e99f97d642c07
Elijah Zhao <elijahzhao24@gmail.com>
2025-08-27 17:50:45 -0600

    test with simple call

--- src/main/java/com/elijahzhao24/NBAsalary_backend/NbAsalaryBackendApplication.java
+++ src/main/java/com/elijahzhao24/NBAsalary_backend/NbAsalaryBackendApplication.java
@@ -2,12 +2,19 @@ package com.elijahzhao24.NBAsalary_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NbAsalaryBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NbAsalaryBackendApplication.class, args);
 	}
 
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
 }
