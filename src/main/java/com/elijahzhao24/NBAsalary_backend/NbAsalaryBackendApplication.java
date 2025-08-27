f181bea4c378d7a6d4abac7aebb2909fed65bd05
Elijah Zhao <elijahzhao24@gmail.com>
2025-08-27 16:37:03 -0600

    Initial commit

---
+++ src/main/java/com/elijahzhao24/NBAsalary_backend/NbAsalaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elijahzhao24.NBAsalary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbAsalaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbAsalaryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elijahzhao24/NBAsalary_backend/NbAsalaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elijahzhao24.NBAsalary_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbAsalaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
